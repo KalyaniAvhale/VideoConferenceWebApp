fe4b349e0c7a485875ae88c540f7cc95679b6560
Kalyani  Avhale <47108982+KalyaniAvhale@users.noreply.github.com>
2022-03-20 20:37:19 +0530

    Update settings.py

--- videochat/settings.py
+++ videochat/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'videochat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
